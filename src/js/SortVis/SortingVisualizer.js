a8417ef7e74ef1b9c7405c9c4df0d67d8bf4b7cc
mikasd <michael.ingley@gmail.com>
2019-10-21 12:22:47 -0700

    fixed merge sort

--- src/js/SortVis/SortingVisualizer.js
+++ src/js/SortVis/SortingVisualizer.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {getMergeSortAnimations} from '../sortingAlgos/sortingAlgos';
+import {getMergeSortAnimations, getBubbleSortAnimations, quickSortFunction} from '../sortingAlgos/sortingAlgos';
+require("regenerator-runtime/runtime");
 
 function randomIntFromInterval(min, max) { // min and max included 
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -25,8 +26,32 @@ class SortingVisualizer extends React.Component{
         this.setState({array});
     }
 
-    mergeSort(){
+    mergeSort() {
         const animations = getMergeSortAnimations(this.state.array);
+        for (let i = 0; i < animations.length; i++) {
+          const arrayBars = document.getElementsByClassName('array-bar');
+          const isColorChange = i % 3 !== 2;
+          if (isColorChange) {
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const color = i % 3 === 0 ? 'red' : 'turquoise';
+            setTimeout(() => {
+              barOneStyle.backgroundColor = color;
+              barTwoStyle.backgroundColor = color;
+            }, i * 3);
+          } else {
+            setTimeout(() => {
+              const [barOneIdx, newHeight] = animations[i];
+              const barOneStyle = arrayBars[barOneIdx].style;
+              barOneStyle.height = `${newHeight}px`;
+            }, i * 3);
+          }
+        }
+      }
+
+    bubbleSort(){
+        const animations = getBubbleSortAnimations(this.state.array);
         for(let i=0;i<animations.length;i++){
             const arrayBars = document.getElementsByClassName('array-bar');
             const isColorChange = i % 3 !== 2;
@@ -38,31 +63,26 @@ class SortingVisualizer extends React.Component{
                 setTimeout(()=>{
                     barOneStyle.backgroundColor = color;
                     barTwoStyle.backgroundColor = color;
-                }, i*3);
+                }, i*1);
             } else {
                 setTimeout(()=>{
                     const [barOneIndex, newHeight] = animations[i];
+                    console.log(newHeight)
                     const barOneStyle = arrayBars[barOneIndex].style;
                     barOneStyle.height = `${newHeight}px`;
-                }, i*3);
+                }, i*1);
             }
         }
-
-        sortingAlgorithms.mergeSort
-    }
-
-    bubbleSort(){
-
     }
 
     quickSort(){
-
+       quickSortFunction(this.state.array, 0, this.state.array.length-1);
     }
 
     heapSort(){
 
     }
-    
+
     testSortingAlgorithms() {
         for (let i = 0; i < 100; i++) {
           const array = [];
@@ -71,8 +91,8 @@ class SortingVisualizer extends React.Component{
             array.push(randomIntFromInterval(-1000, 1000));
           }
           const javaScriptSortedArray = array.slice().sort((a, b) => a - b);
-          const mergeSortedArray = getMergeSortAnimations(array.slice());
-          console.log(arraysAreEqual(javaScriptSortedArray, mergeSortedArray));
+          const bubbleSortedArray = getBubbleSortAnimations(array.slice());
+          console.log(arraysAreEqual(javaScriptSortedArray, bubbleSortedArray));
         }
       }
     
@@ -95,6 +115,7 @@ class SortingVisualizer extends React.Component{
          <button onClick={()=>this.bubbleSort()}>bubbleSort</button>
          <button onClick={()=>this.quickSort()}>quickSort</button>
          <button onClick={()=>this.heapSort()}>heapSort</button>
+         <button onClick={()=>this.testSortingAlgorithms()}>test</button>
         </div>
         </>
     )
--- src/js/sortingAlgos/sortingAlgos.js
+++ src/js/sortingAlgos/sortingAlgos.js
@@ -76,6 +76,7 @@ export function getMergeSortAnimations(array) {
       mainArray[k++] = auxiliaryArray[j++];
     }
   }
+
 // export const mergeSort = array => {
 //     if(array.length === 1) return array;
 //     const middleIndex = Math.floor(array.length/2);
@@ -95,3 +96,76 @@ export function getMergeSortAnimations(array) {
 //     while(j<secondHalf.length) sortedArray.push(secondHalf[j++]);
 //     return sortedArray;
 // }
+
+export function getBubbleSortAnimations(arr){
+    const bubbleSortAnimations = [];
+          for (let i = 0; i < arr.length; i++) {
+              for(let j=0;j<arr.length-i-1;j++){
+                if (arr[j] > arr[j + 1]) {
+                let index=arr.length-i-1;
+                bubbleSortAnimations.push([j,j+1])
+                bubbleSortAnimations.push([j,j+1])
+                let tmp = arr[j];
+                arr[j] = arr[j + 1];
+                bubbleSortAnimations.push([index,tmp])
+                arr[j + 1] = tmp;
+                }
+            }
+          }
+          console.log(arr);
+    return bubbleSortAnimations
+};
+
+// QUICK SORT==============================================
+
+//takes leftIndex Item and swaps it with rightIndex item.
+export async function quickSortFunction(arr, start, end) {
+    if (start >= end) {
+      return;
+    }
+    let index = await partition(arr, start, end);
+    // states[index] = -1;
+  
+    await Promise.all([
+      quickSortFunction(arr, start, index - 1),
+      quickSortFunction(arr, index + 1, end)
+    ]);
+  }
+  
+  async function partition(arr, start, end) {
+    for (let i = start; i < end; i++) {
+    //   states[i] = 1;
+    }
+  
+    let pivotValue = arr[end];
+    let pivotIndex = start;
+    // states[pivotIndex] = 0;
+    for (let i = start; i < end; i++) {
+      if (arr[i] < pivotValue) {
+        await swap(arr, i, pivotIndex);
+        // states[pivotIndex] = -1;
+        pivotIndex++;
+        // states[pivotIndex] = 0;
+      }
+    }
+    await swap(arr, pivotIndex, end);
+  
+    for (let i = start; i < end; i++) {
+      if (i != pivotIndex) {
+        // states[i] = -1;
+      }
+    }
+  
+    return pivotIndex;
+  }
+  
+  async function swap(arr, a, b) {
+    await sleep(50);
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+  
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
